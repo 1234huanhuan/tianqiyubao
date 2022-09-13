073954103d99b9985848cfac266eba039c2583f0
1234huanhuan <112237032+1234huanhuan@users.noreply.github.com>
2022-09-13 22:26:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app_id = os.getenv('APP_ID')
 app_secret = os.getenv('APP_SECRET')
 
 user_ids = os.getenv('USER_ID', '').split("\n")
-url = "https://lab.magiconch.com/sakana/?v=takina"
+url = "https://lab.magiconch.com/sakana/"
 template_id = os.getenv('TEMPLATE_ID')
 
 # 为读取农历生日准备
